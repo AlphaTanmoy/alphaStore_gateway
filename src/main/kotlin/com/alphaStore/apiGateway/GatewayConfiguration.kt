c1e4a3768480a1b068e427297c91e4b95f41ade4
AlphaTanmoy <69491103+AlphaTanmoy@users.noreply.github.com>
2024-11-25 14:37:07 +0530

    test changes

--- src/main/kotlin/com/alphaStore/apiGateway/GatewayConfiguration.kt
+++ src/main/kotlin/com/alphaStore/apiGateway/GatewayConfiguration.kt
@@ -11,26 +11,32 @@ class GatewayConfiguration {
     @Bean
     fun routes(builder: RouteLocatorBuilder): RouteLocator {
         return builder.routes()
+
             .route("cart") { r ->
                 r.path("/cart/**")
                     .uri("http://localhost:8083")
             }
+
             .route("merchant") { r ->
                 r.path("/merchant/**")
                     .uri("http://localhost:8084")
             }
+
             .route("product") { r ->
                 r.path("/product/**")
                     .uri("http://localhost:8085")
             }
+
             .route("seeder") { r ->
                 r.path("/seeder/**")
                     .uri("http://localhost:8086")
             }
+
             .route("user") { r ->
                 r.path("/user/**")
                     .uri("http://localhost:8087")
             }
+
             .route("wallet") { r ->
                 r.path("/wallet/**")
                     .uri("http://localhost:8088")
